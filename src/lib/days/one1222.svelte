3b6914e14cc251f80bd4cf1f4337286209450886
Brendan <bcowley@fusetg.com>
2022-01-12 16:37:54 -0500

    added 20220112

---
+++ src/lib/days/one1222.svelte
@@ -0,0 +1,289 @@
+<h2>1-12-2022</h2>
+<section class="dailySection">
+	<div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://gsiexchange.com/banks-amend-depositor-agreement-making-it-easier-to-seize-your-money-on-a-hidden-technical-oversight/"
+		>
+        <span class="imp">Chase, Citi, BofA, and US Bank, Amend Agreement on Bank Failure</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://alexepstein.substack.com/p/the-esg-movement-is-anti-energy-anti"
+		>
+			<span class="imp">The ESG movement is anti-energy, anti-development, and anti-America</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.forbes.com/sites/roxannerobinson/2022/01/11/fashion-industry-reacts-to-new-york-sustainability-legislation-that-could-upend-transparency-practices/?sh=349c1ef39b33"
+		>
+			<span class='imp'>Fashion Industry Reacts To New York Sustainability Legislation That Could Upend Transparency Practices</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.enr.com/articles/51886-waters-of-the-us-rule-to-be-rewritten-yet-again"
+		>
+			<span class="imp">Waters of the U.S. Rule to Be Rewritten, Yet Again</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.vox.com/2021/5/7/22423139/biden-30-by-30-conservation-initiative-historic"
+		>
+			<span class="imp">The Biden administration has a game-changing approach to nature conservation</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.jswconline.org/content/67/2/37A"
+		>
+			<span class="imp">Enhancing ecosystem services: Designing for multifunctionality</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://unusualwhales.com/i_am_the_senate/full"
+		>
+			<span class="imp">Congressional Trading in 2021</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://mronline.org/2021/05/21/this-biden-proposal-could-make-the-u-s-a-digital-dictatorship/"
+		>
+        This Biden proposal could make the U.S. a “digital dictatorship”
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://medium.com/blockchain/the-coming-us-digital-dollar-part-1-what-it-is-and-why-it-matters-a8a92d8e4eef"
+		>
+        The Coming US Digital Dollar (Part 1) — What it is, and Why it Matters
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.foxnews.com/politics/education-secretary-cardona-solicited-nsba-letter-comparing-parents-domestic-terrorists-email"
+		>
+			<span class="imp">Education Secretary Cardona solicited NSBA letter comparing protesting parents to domestic terrorists: email</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://thenationalpulse.com/2021/08/23/wuhan-filed-patent-for-lab-accident/"
+		>
+        Wuhan Lab Filed Patent For Treating ‘Accidental Exposure’ To Deadly Pathogens Just 1 Month Before COVID-19 Outbreak.
+		</a>
+	</div>
+    
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://finance.yahoo.com/news/imf-crypto-not-fringe-anymore-link-to-stocks-may-pose-systemic-risks-211121524.html"
+		>
+        Yahoo Finance
+        IMF: Crypto not ‘fringe’ anymore, link to stocks may pose systemic risks
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://thepostmillennial.com/ronald-mcdonald-house-to-evict-families-who-are-not-vaccinated?utm_campaign=64487"
+		>
+        Ronald McDonald House to evict families with young children who are unvaccinated
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.documentcloud.org/documents/7014520-Fauci-Disclosure"
+		>
+			<span class="imp">Fauci Public Financial Disclosure Report</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.zerohedge.com/markets/stocks-vs-bonds-what-could-go-wrong"
+		>
+        Stocks Vs. Bonds - What Could Go Wrong?
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.bloomberg.com/news/articles/2022-01-11/repeat-booster-shots-risk-overloading-immune-system-ema-says"
+		>
+        Frequent Boosters Spur Warning on Immune Response
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.wsj.com/articles/sec-gives-more-investors-access-to-private-equity-hedge-funds-11598452858?mod=article_inline"
+		>
+        SEC Gives More Investors Access to Private Equity, Hedge Funds
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.cyberscoop.com/feds-spending-on-facial-recognition-tech-continues-unmitigated-despite-privacy-concerns/"
+		>
+        Feds' spending on facial recognition tech expands, despite privacy concerns
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.wsj.com/articles/the-end-of-car-keys-passwords-and-fumbling-with-your-phone-at-checkout-11641618007?mod=djemalertNEWS"
+		>
+        The End of Car Keys, Passwords and Fumbling With Your Phone at Checkout
+		</a>
+	</div>
+	<div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://justthenews.com/politics-policy/education/parents-rally-ahead-michigan-school-board-meeting-over-woke-districts"
+		>
+        Michigan parents protest racial program, school board doubles down anyway
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.motortrader.com/blogs/pandemic-sees-rise-demand-sub-prime-finance-23-03-2021"
+		>
+        Pandemic sees rise in demand for sub-prime finance
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.thelocal.fr/20210920/french-electricity-prices-could-skyrocket-in-2022-consumer-group-warns/"
+		>
+        French electricity prices could skyrocket in 2022, consumer group warns
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://laws-lois.justice.gc.ca/eng/acts/e-4.5/page-1.html#h-213849"
+		>
+			Canadian Totalitarianism 
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.washingtonexaminer.com/policy/leaked-photos-reveal-bidens-disastrous-detention-of-migrants-in-arizona"
+		>
+        Leaked photos reveal Biden's disastrous detention of migrants in Arizona
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.judicialwatch.org/documents/jw-v-hhs-wuhan-august-31-2021-00696/"
+		>
+        JW v HHS Wuhan August 31 2021 00696
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.theverge.com/2022/1/10/22877304/google-lockheimer-accuse-apple-imessage-holding-back-rcs-text-sms?scrolla=5eb6d68b7fedc32c19ef33b4"
+		>
+        Google exec says Apple is ‘holding back’ customers who text
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.judicialwatch.org/walensky-cdc-records/"
+		>
+        Judicial Watch: New CDC Records Detail Agency Head Walensky Wanted Details of Death of Teenager Reported to Vaccine Adverse Event Reporting System
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://technofog.substack.com/p/new-documents-reveal-early-beliefs"
+		>
+        New documents reveal early beliefs that SARS-CoV-2 was engineered
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.scmp.com/news/china/science/article/3162972/china-has-built-artificial-moon-simulates-low-gravity-conditions"
+		>
+        China has built an artificial moon that simulates low-gravity conditions on Earth
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.wsj.com/articles/the-greening-of-the-federal-reserve-jerome-powell-climate-11641941000?st=j22ws7up849hnl3&reflink=desktopwebshare_twitter"
+		>
+        The Greening of the Federal Reserve
+		</a>
+	</div>
+    
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.zerohedge.com/commodities/pig-ebola-outbreak-imminent-europe"
+		>
+        Is A Pig Ebola Outbreak Imminent In Europe?
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.wsj.com/articles/us-inflation-consumer-price-index-december-2021-11641940760"
+		>
+			<span class="imp">U.S. Inflation Hit 7% in December, Fastest Pace Since 1982</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.dailysignal.com/2022/01/11/breaking-biden-administration-making-lists-of-religious-vaccine-objectors/"
+		>
+			<span class="imp">BREAKING: Biden Administration Making Lists of Religious Vaccine Objectors</span>
+		</a>
+	</div>
+    <div class="storyWrapper">
+		<a
+			class="storyLink"
+			href="https://www.lifenews.com/2022/01/11/catholic-governor-phil-murphy-will-sign-bill-legalizing-abortions-up-to-birth-in-new-jersey/"
+		>
+        Catholic Governor Phil Murphy Will Sign Bill Legalizing Abortions Up to Birth in New Jersey
+		</a>
+	</div>
+
+</section>
+
+<style>
+	.storyWrapper {
+		/* border-bottom: solid 1px lightgrey; */
+		padding: 1em;
+	}
+    .imp {
+        color: red;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
     import OneEight22ToOneTen22 from '../lib/days/oneEight22ToOneTen22.svelte'
     import OneEleven22 from '$lib/days/oneEleven22.svelte'
+    import One1222 from '$lib/days/one1222.svelte';
 </script>
 <h1>The Daily Brief will be in descending order. Stay Frosty.</h1>
 
+<One1222/>
 <OneEleven22/>
 <OneEight22ToOneTen22/>
 
