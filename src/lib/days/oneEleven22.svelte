84094c2206fa8c8a33c3581778148d648bf3e3f8
Brendan <bcowley@fusetg.com>
2022-01-12 16:51:25 -0500

    put everything in details

--- src/lib/days/oneEleven22.svelte
+++ src/lib/days/oneEleven22.svelte
@@ -1,243 +1,248 @@
-<h2>1-11-2022</h2>
+
+
 <section class="dailySection">
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://assets.ctfassets.net/syq3snmxclc9/2mVob3c1aDd8CNvVnyei6n/95af7dbfd2958d4c2b8494048b4889b5/JAG_Docs_pt1_Og_WATERMARK_OVER_Redacted.pdf"
-		>
-			Murphy Darpa Docs
-			<p class="excerpt" >Important</p>
-		</a>
-	</div>
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://assets.ctfassets.net/syq3snmxclc9/6K3RxB1DVf6ZhVxQLSJzxl/6be5c276bc8af7921ce6b23f0975a6c3/A_prempt-background-hr001118s0017.pdf"
-		>
-			Darpa PREEMPT Docs
-			<p class="excerpt" >Important</p>
-		</a>
-	</div>
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://assets.ctfassets.net/syq3snmxclc9/5OjsrkkXHfuHps6Lek1MO0/5e7a0d86d5d67e8d153555400d9dcd17/defuse-project-rejection-by-darpa.pdf"
-		>
-			Darpa DEFUSE Rejection Docs
-			<p class="excerpt" >Important</p>
-		</a>
-	</div>
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.projectveritas.com/news/military-documents-about-gain-of-function-contradict-fauci-testimony-under/"
-		>
-			Project Veritas Fauci Story
-			<p class="excerpt" >Important</p>
-		</a>
-	</div>
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://ec.europa.eu/assets/epsc/pages/espas/chapter1.html"
-		>
-			Europe's Sales Pitch On Great Reset
-			<p class="excerpt" >Important</p>
-		</a>
-	</div>
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.nature.com/articles/s41562-020-0884-z"
-		>
-        Using social and behavioural science to support COVID-19 pandemic response
-			<p class="excerpt">
-				Important Read
-			</p>
-		</a>
-	</div>
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://today.law.harvard.edu/cass-sunstein-tapped-to-chair-who-technical-advisory-group/"
-		>
-        Cass Sunstein tapped to chair WHO technical advisory group
-			<p class="excerpt" >
-                His name should be familiar... for all the wrong reasons.
-            </p>
-		</a>
-	</div>
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.usni.org/magazines/proceedings/2022/january/drinking-fetid-well-data-poisoning-and-machine-learning"
-			><strong>Drinking from the Fetid Well: Data Poisoning and Machine Learning</strong>
-			<p class="excerpt" />
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.washingtonexaminer.com/restoring-america/community-family/bidens-cdc-director-says-theres-no-excuse-for-keeping-schools-closed-only-teachers-unions-missed-the-memo"
-		>
-        CDC director says there's no excuse for keeping schools closed
-			<p class="excerpt">
-				
-			</p>
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.newsmax.com/newsfront/nancy-pelosi-big-tech-stocks/2022/01/08/id/1051563/"
-		>
-        NY Post: Pelosi Makes Up to $30 Million on Insider Stock Trades
-			<p class="excerpt" />
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://timesofindia.indiatimes.com/home/science/t-cells-triggered-by-common-cold-also-fend-off-covid-study/articleshow/88810374.cms"
-		>
-        T cells triggered by common cold also fend off Covid: Study
-
-        Read more at:
-        http://timesofindia.indiatimes.com/articleshow/88810374.cms?utm_source=contentofinterest&utm_medium=text&utm_campaign=cppst
-			<p class="excerpt" />
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.studyfinds.org/meditating-genes-cancer-covid/"
-		>
-        Meditating each day activates genes that fight off cancer and viruses like COVID-19
-			<p class="excerpt">
-			</p>
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.judicialwatch.org/bidens-benghazi/"
-		>
-			Biden's Benghazi
-			<p class="excerpt" />
-		</a>
-	</div>
-
+	<details>
+		<summary><strong>1-11-2022</strong></summary>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://assets.ctfassets.net/syq3snmxclc9/2mVob3c1aDd8CNvVnyei6n/95af7dbfd2958d4c2b8494048b4889b5/JAG_Docs_pt1_Og_WATERMARK_OVER_Redacted.pdf"
+			>
+				Murphy Darpa Docs
+				<p class="excerpt" >Important</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://assets.ctfassets.net/syq3snmxclc9/6K3RxB1DVf6ZhVxQLSJzxl/6be5c276bc8af7921ce6b23f0975a6c3/A_prempt-background-hr001118s0017.pdf"
+			>
+				Darpa PREEMPT Docs
+				<p class="excerpt" >Important</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://assets.ctfassets.net/syq3snmxclc9/5OjsrkkXHfuHps6Lek1MO0/5e7a0d86d5d67e8d153555400d9dcd17/defuse-project-rejection-by-darpa.pdf"
+			>
+				Darpa DEFUSE Rejection Docs
+				<p class="excerpt" >Important</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.projectveritas.com/news/military-documents-about-gain-of-function-contradict-fauci-testimony-under/"
+			>
+				Project Veritas Fauci Story
+				<p class="excerpt" >Important</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://ec.europa.eu/assets/epsc/pages/espas/chapter1.html"
+			>
+				Europe's Sales Pitch On Great Reset
+				<p class="excerpt" >Important</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.nature.com/articles/s41562-020-0884-z"
+			>
+			Using social and behavioural science to support COVID-19 pandemic response
+				<p class="excerpt">
+					Important Read
+				</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://today.law.harvard.edu/cass-sunstein-tapped-to-chair-who-technical-advisory-group/"
+			>
+			Cass Sunstein tapped to chair WHO technical advisory group
+				<p class="excerpt" >
+					His name should be familiar... for all the wrong reasons.
+				</p>
+			</a>
+		</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.usni.org/magazines/proceedings/2022/january/drinking-fetid-well-data-poisoning-and-machine-learning"
+				><strong>Drinking from the Fetid Well: Data Poisoning and Machine Learning</strong>
+				<p class="excerpt" />
+			</a>
+		</div>
 	
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.rfa.org/english/news/china/surge-12232021120335.html"
-		>
-        China's Xi'an under lockdown amid surge in COVID-19, hemorrhagic fever cases
-			<p class="excerpt">
-				
-			</p>
-		</a>
-	</div>
-
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.washingtonexaminer.com/restoring-america/community-family/bidens-cdc-director-says-theres-no-excuse-for-keeping-schools-closed-only-teachers-unions-missed-the-memo"
+			>
+			CDC director says there's no excuse for keeping schools closed
+				<p class="excerpt">
+					
+				</p>
+			</a>
+		</div>
 	
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.msn.com/en-ca/news/world/in-cuba-queueing-for-hours-just-to-be-able-to-eat/ar-AASDUsE?pfr=1"
-		>
-        Cuba is running out of everything
-			<p class="excerpt" />
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://hotair.com/jazz-shaw/2022/01/11/la-times-its-necessary-to-mock-the-deaths-of-the-unvaccinated-n440636"
-		>
-        LA Times: It's necessary to mock the deaths of the unvaccinated
-			<p class="excerpt" />
-		</a>
-	</div>
-
-	<div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://pjmedia.com/columns/megan-fox/2022/01/10/the-branch-covidians-are-basically-the-same-as-the-heavens-gate-cultists-except-not-as-nice-n1548213"
-		>
-        The Branch Covidians Are Basically the Same as the Heaven's Gate Cultists (Except Not as Nice)
-			<p class="excerpt"></p>
-		</a>
-	</div>
- 
-
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.washingtonpost.com/national-security/domestic-terrorism-justice-threat/2022/01/11/dfd8d82c-72eb-11ec-8b0a-bcfab800c430_story.html"
-		>
-        Justice Dept. forms new domestic terrorism unit to address growing threat
-			<p class="excerpt" ></p>
-		</a>
-	</div>
-
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.thedrive.com/the-war-zone/43828/undersea-cable-connecting-norway-with-arctic-satellite-station-has-been-mysteriously-severed"
-		>
-        Undersea Cable Connecting Norway With Arctic Satellite Station Has Been Mysteriously Severed
-			<p class="excerpt" ></p>
-		</a>
-	</div>
-
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.usnews.com/news/world/articles/2022-01-10/china-offers-kazakhstan-security-support-opposes-external-forces"
-		>
-        China Offers Kazakhstan Security Support, Opposes 'External Forces'
-			<p class="excerpt" ></p>
-		</a>
-	</div>
-
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.zerohedge.com/covid-19/covid-survival-rate-under-20s-999987-stanford-epidemiolgy-prof-study"
-		>
-        COVID Survival Rate For Under 20s Is 99.9987%; Stanford Epidemiology Prof Study
-			<p class="excerpt" ></p>
-		</a>
-	</div>
-
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://theconservativetreehouse.com/blog/2020/03/20/fauci-love-letters-to-hillary-clinton-surfaces/"
-		>
-        Fauci Love Letters to Hillary Clinton Surface
-			<p class="excerpt" ></p>
-		</a>
-	</div>
-
-    <div class="storyWrapper">
-		<a
-			class="storyLink"
-			href="https://www.foxnews.com/politics/education-secretary-cardona-solicited-nsba-letter-comparing-parents-domestic-terrorists-email"
-		>
-        Education Secretary Cardona solicited NSBA letter comparing protesting parents to domestic terrorists: email
-			<p class="excerpt" ></p>
-		</a>
-	</div>
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.newsmax.com/newsfront/nancy-pelosi-big-tech-stocks/2022/01/08/id/1051563/"
+			>
+			NY Post: Pelosi Makes Up to $30 Million on Insider Stock Trades
+				<p class="excerpt" />
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://timesofindia.indiatimes.com/home/science/t-cells-triggered-by-common-cold-also-fend-off-covid-study/articleshow/88810374.cms"
+			>
+			T cells triggered by common cold also fend off Covid: Study
+	
+			Read more at:
+			http://timesofindia.indiatimes.com/articleshow/88810374.cms?utm_source=contentofinterest&utm_medium=text&utm_campaign=cppst
+				<p class="excerpt" />
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.studyfinds.org/meditating-genes-cancer-covid/"
+			>
+			Meditating each day activates genes that fight off cancer and viruses like COVID-19
+				<p class="excerpt">
+				</p>
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.judicialwatch.org/bidens-benghazi/"
+			>
+				Biden's Benghazi
+				<p class="excerpt" />
+			</a>
+		</div>
+	
+		
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.rfa.org/english/news/china/surge-12232021120335.html"
+			>
+			China's Xi'an under lockdown amid surge in COVID-19, hemorrhagic fever cases
+				<p class="excerpt">
+					
+				</p>
+			</a>
+		</div>
+	
+		
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.msn.com/en-ca/news/world/in-cuba-queueing-for-hours-just-to-be-able-to-eat/ar-AASDUsE?pfr=1"
+			>
+			Cuba is running out of everything
+				<p class="excerpt" />
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://hotair.com/jazz-shaw/2022/01/11/la-times-its-necessary-to-mock-the-deaths-of-the-unvaccinated-n440636"
+			>
+			LA Times: It's necessary to mock the deaths of the unvaccinated
+				<p class="excerpt" />
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://pjmedia.com/columns/megan-fox/2022/01/10/the-branch-covidians-are-basically-the-same-as-the-heavens-gate-cultists-except-not-as-nice-n1548213"
+			>
+			The Branch Covidians Are Basically the Same as the Heaven's Gate Cultists (Except Not as Nice)
+				<p class="excerpt"></p>
+			</a>
+		</div>
+	 
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.washingtonpost.com/national-security/domestic-terrorism-justice-threat/2022/01/11/dfd8d82c-72eb-11ec-8b0a-bcfab800c430_story.html"
+			>
+			Justice Dept. forms new domestic terrorism unit to address growing threat
+				<p class="excerpt" ></p>
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.thedrive.com/the-war-zone/43828/undersea-cable-connecting-norway-with-arctic-satellite-station-has-been-mysteriously-severed"
+			>
+			Undersea Cable Connecting Norway With Arctic Satellite Station Has Been Mysteriously Severed
+				<p class="excerpt" ></p>
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.usnews.com/news/world/articles/2022-01-10/china-offers-kazakhstan-security-support-opposes-external-forces"
+			>
+			China Offers Kazakhstan Security Support, Opposes 'External Forces'
+				<p class="excerpt" ></p>
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.zerohedge.com/covid-19/covid-survival-rate-under-20s-999987-stanford-epidemiolgy-prof-study"
+			>
+			COVID Survival Rate For Under 20s Is 99.9987%; Stanford Epidemiology Prof Study
+				<p class="excerpt" ></p>
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://theconservativetreehouse.com/blog/2020/03/20/fauci-love-letters-to-hillary-clinton-surfaces/"
+			>
+			Fauci Love Letters to Hillary Clinton Surface
+				<p class="excerpt" ></p>
+			</a>
+		</div>
+	
+		<div class="storyWrapper">
+			<a
+				class="storyLink"
+				href="https://www.foxnews.com/politics/education-secretary-cardona-solicited-nsba-letter-comparing-parents-domestic-terrorists-email"
+			>
+			Education Secretary Cardona solicited NSBA letter comparing protesting parents to domestic terrorists: email
+				<p class="excerpt" ></p>
+			</a>
+		</div>
+	</details>
+    
 
 </section>
 
